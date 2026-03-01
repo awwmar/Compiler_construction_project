5b271faa7dfb223ff5ab02de1cb22ad934aeac2e
Amar Kumar <142306355+awwmar@users.noreply.github.com>
2026-03-01 18:46:04 +0530

    Revise comments in lexer.h for better understanding
    Updated comments in lexer.h for clarity.

--- Group_38/lexer.h
+++ Group_38/lexer.h
@@ -8,16 +8,16 @@
 #include <stdio.h>
 #include "lexerDef.h"
 
-/* Initialize twin buffer and load first chunk */
+// Start the reader and grab the first chunk of code
 FILE *getStream(FILE *fp, TwinBuffer *B);
 
-/* Get next token from twin buffer */
+// Get the next token from the pile
 TokenInfo getNextToken(TwinBuffer *B);
 
-/* Remove comments and print clean code to console */
+// Delete comments and just print the clean stuff
 void removeComments(char *testcaseFile);
 
-/* Get string name of a token type */
+// Change a code label into a name people can actually read
 const char *tokenTypeToString(TokenType t);
 
 #endif
